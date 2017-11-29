e5babeff71935030ccf52b80aa9eed5a3b4ed573
Eddie Pedersen <eddiepedersen90@gmail.com>
2017-11-29 14:51:16 +0000

    twitter key

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
-}
+  consumer_key: '<dmN8aYfRewABQv9fwzucrDwC1>',
+  consumer_secret: '<	Lz8lpYGS1I5Ke0ZWTSmyVqydCmXLsboKnrBNynxSXpPSz6dLKD>',
+  access_token_key: '<	935696673694781440-xmg8YekpU8gHimTaHxB7F9Li4UIcehi>',
+  access_token_secret: '<yfdTrcQgsLzhIF6og5tY47QexYtdXslbekwwryFgzVpes>',
+};
 
 module.exports = twitterKeys;
